0e71fdc3b0197ae5c3156aabb13d8c6e95542f75
cedanosergio11 <cedanosergio11@gmail.com>
2026-09-01 16:40:50 -0500

    feat: honor Vite BASE_URL as TanStack Router basepath

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,13 @@ import { createRouter } from "@tanstack/react-router";
 import { AppErrorComponent } from "@/lib/error-component";
 import { routeTree } from "./routeTree.gen";
 
+const viteBase = import.meta.env.BASE_URL;
+const basepath = viteBase === "/" ? undefined : viteBase.replace(/\/$/, "");
+
 export function getRouter() {
-  return createRouter({ routeTree, defaultErrorComponent: AppErrorComponent });
+  return createRouter({
+    routeTree,
+    defaultErrorComponent: AppErrorComponent,
+    ...(basepath ? { basepath } : {}),
+  });
 }
